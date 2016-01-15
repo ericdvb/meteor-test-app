fbb676879b38ae9ff2b5ba836d1b77baab586526
Eric Willenson <eric.willenson@gmail.com>
2016-01-15 16:21:40 -0500

    Add menu styling and logic

---
+++ client/logic/menu.js
@@ -0,0 +1,26 @@
+Template.menu.helpers({
+  firstIndex: function(index) {
+    if(index == 0) {
+      return true;
+    }
+  },
+
+  menuItems: [
+    {
+      title: 'privacy policy',
+      href: '#'
+    },
+    {
+      title: 'terms of use',
+      href: '#'
+    },
+    {
+      title: 'settings',
+      href: '#'
+    },
+    {
+      title: 'logout',
+      href: '#'
+    }
+  ]
+});
